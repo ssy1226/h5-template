146f18d0c875c67cb89932a47f310b9f4790f210
Shuyu Shi (IT) <shuyu.shi@cicc.com.cn>
2021-12-16 06:27:20 +0000

    Feature/cockpit

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import user from './user'
 import login from './login'
+import theme from './theme'
 
 export default combineReducers({
   user,
-  login
+  login,
+  theme
 })
